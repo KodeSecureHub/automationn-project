8a3fe09c22fe698b692a7ec9c34ca7a040666ca4
KodeSecureHub <kodeeswaranas503@gmail.com>
2026-03-17 12:24:53 +0530

    "Automation Project"

--- src/Login/BaseClass.java
+++ src/Login/BaseClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-class LoginBaseClass{
+class BaseClass {
     public static WebElement userName(WebDriver driver)
     {
         return driver.findElement(By.id("username"));
--- src/Login/TestClass.java
+++ src/Login/TestClass.java
@@ -2,15 +2,15 @@ package Login;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-public class LoginTestClass {
+public class TestClass {
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://jpetstore.aspectran.com/account/signonForm");
-        LoginBaseClass.userName(driver).clear();
-        LoginBaseClass.userName(driver).sendKeys("kodi");
-        LoginBaseClass.passWord(driver).clear();
-        LoginBaseClass.passWord(driver).sendKeys("1234");
-        LoginBaseClass.submit(driver).click();
+        BaseClass.userName(driver).clear();
+        BaseClass.userName(driver).sendKeys("kodi");
+        BaseClass.passWord(driver).clear();
+        BaseClass.passWord(driver).sendKeys("1234");
+        BaseClass.submit(driver).click();
     }
 }
