4e22f9fff95c1ee382ff2b1cd4b2d3ce2c446041
Ramprakash100 <ramvairavaraj@gmail.com>
2026-03-19 13:50:09 +0530

    UpdateCart

---
+++ src/UpdateCart/BaseClass.java
@@ -0,0 +1,52 @@
+package UpdateCart;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BaseClass {
+    @FindBy(linkText = "Fish")
+    public static WebElement fish1;
+    @FindBy(linkText = "FI-SW-02")
+    public static WebElement product_id1;
+
+    @FindBy(linkText = "Birds")
+    public static WebElement bird1;
+    @FindBy(linkText = "AV-CB-01")
+    public static WebElement product_id2;
+
+
+    @FindBy(linkText = "Dogs")
+    public static WebElement dog1;
+    @FindBy(linkText = "K9-BD-01")
+    public static WebElement product_id3;
+
+    @FindBy(linkText = "Cats")
+    public static WebElement cat1;
+    @FindBy(linkText = "FL-DLH-02")
+    public static WebElement product_id4;
+
+    @FindBy(linkText = "Reptiles")
+    public static WebElement reptile1;
+    @FindBy(linkText = "RP-SN-01")
+    public static WebElement product_id5;
+    @FindBy(linkText = "Add to Cart")
+    public static WebElement cart;
+    @FindBy(linkText = "Return to Main Menu")
+    public static WebElement menu;
+
+    @FindBy(linkText = "EST-3")
+    public static WebElement update1;
+    @FindBy(linkText = "EST-18")
+    public static WebElement update2;
+    @FindBy(linkText = "EST-6")
+    public static WebElement update3;
+    @FindBy(linkText = "EST-16")
+    public static WebElement update4;
+    @FindBy(linkText = "EST-11")
+    public static WebElement update5;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public static WebElement updateClick;
+}
---
+++ src/UpdateCart/TestClass.java
@@ -0,0 +1,53 @@
+package UpdateCart;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestClass {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://jpetstore.aspectran.com/");
+        driver.manage().window().maximize();
+
+        PageFactory.initElements(driver, addtocart.BaseClass.class);
+
+        BaseClass.fish1.click();
+        BaseClass.product_id1.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+        BaseClass.bird1.click();
+        BaseClass.product_id2.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+        BaseClass.dog1.click();
+        BaseClass.product_id3.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+
+        BaseClass.cat1.click();
+        BaseClass.product_id4.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+        BaseClass.reptile1.click();
+        BaseClass.product_id5.click();
+        BaseClass.cart.click();
+
+        BaseClass.update1.clear();
+        BaseClass.update1.sendKeys("2");
+        BaseClass.update2.clear();
+        BaseClass.update2.sendKeys("3");
+        BaseClass.update3.clear();
+        BaseClass.update3.sendKeys("2");
+        BaseClass.update4.clear();
+        BaseClass.update4.sendKeys("4");
+        BaseClass.update5.clear();
+        BaseClass.update5.sendKeys("3");
+
+        BaseClass.updateClick.clear();
+
+    }
+}
