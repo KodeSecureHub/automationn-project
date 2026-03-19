f8c1697f202a4d38fd8ab2db83489870223ea53c
Ramprakash100 <ramvairavaraj@gmail.com>
2026-03-19 13:06:56 +0530

    Cart updated

--- src/addtocart/TestClass.java
+++ src/addtocart/TestClass.java
@@ -9,30 +9,33 @@ public class TestClass {
         driver.get("https://jpetstore.aspectran.com/");
         driver.manage().window().maximize();
 
-        BaseClass.fish1(driver).click();
-        BaseClass.productId1(driver).click();
-        BaseClass.cart1(driver).click();
-        BaseClass.menu1(driver).click();
-
-        BaseClass.bird1(driver).click();
-        BaseClass.product_id2(driver).click();
-        BaseClass.cart2(driver).click();
-        BaseClass.menu2(driver).click();
-
-        BaseClass.dog1(driver).click();
-        BaseClass.product_id3(driver).click();
-        BaseClass.cart3(driver).click();
-        BaseClass.menu3(driver).click();
-
         PageFactory.initElements(driver,BaseClass.class);
+
+        BaseClass.fish1.click();
+        BaseClass.product_id1.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+        BaseClass.bird1.click();
+        BaseClass.product_id2.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+        BaseClass.dog1.click();
+        BaseClass.product_id3.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
+
         BaseClass.cat1.click();
         BaseClass.product_id4.click();
-        BaseClass.cart4.click();
-        BaseClass.menu4.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
 
         BaseClass.reptile1.click();
         BaseClass.product_id5.click();
-        BaseClass.cart4.click();
-        BaseClass.menu5.click();
+        BaseClass.cart.click();
+        BaseClass.menu.click();
+
     }
 }
